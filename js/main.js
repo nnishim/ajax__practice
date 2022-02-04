f9b04d616df8295c4a7958bcf88a915eeb97b2de
nnishim <92303619+nnishim@users.noreply.github.com>
2022-02-04 13:35:10 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const options = {
+  url: `https://restcountries.com/v2/all?fields=name,flag`,
+  getValue: `name`,
+  list: {
+    match:{
+      enabled: true
+    }
+  },
+  template: {
+		type: "custom",
+		method: function (value, item) {
+      return `
+      <span style="color: tomato">${value}</span> 
+      <img src="${item.flag}" alt="${item.name}" style="width: 20px"></img>
+      `
+    }
+	}
+};
+
+$(`#name`).easyAutocomplete(options);
